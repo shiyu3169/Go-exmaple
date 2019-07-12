f881ef0d30501963a43630d3a880b46dd293081f
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 14:43:26 -0400

    array

---
+++ 05_arrays_slices/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// // Arrays
+	// var fruitArr [2]string
+
+	// // Assign values
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Orange"
+
+	// Declare and assign
+	fruitArr := [2]string{"Apple", "Orange"}
+
+	fmt.Println(fruitArr)
+	fmt.Println(fruitArr[1])
+}
