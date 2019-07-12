6200ff2c056cc8c2f57284e64173b1f391d10b4c
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 13:29:31 -0400

    const

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -6,7 +6,8 @@ func main() {
 	// Using var
 	var name = "Shiyu"
 	var age = 27
+	const isCool = true
 
-	fmt.Println(name, age)
-	fmt.Printf("%T\n", name)
+	fmt.Println(name, age, isCool)
+	fmt.Printf("%T\n", isCool)
 }
