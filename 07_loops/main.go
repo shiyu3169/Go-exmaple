464ceee40f1adb9a9bcc286ef66d9e92a804f4ad
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 15:31:42 -0400

    fizzbuzz

--- 07_loops/main.go
+++ 07_loops/main.go
@@ -14,6 +14,19 @@ func main() {
 
 	// Short method
 	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
+		fmt.Printf("Number %d\n", i)
 	}
+
+	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+
 }
