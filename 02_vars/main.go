db5886bf0876acbb2225e974b73edaa294b75ce9
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 13:40:14 -0400

    float

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -4,13 +4,19 @@ import "fmt"
 
 func main() {
 	// Using var
-
 	var age = 27
+
 	// Const
 	const isCool = true
+
 	// Shorthand
 	name := "Shiyu"
 
-	fmt.Println(name, age, isCool)
+	// Float
+	size := 1.3
+
+	fmt.Println(name, age, isCool, size)
+
+	// Print type
 	fmt.Printf("%T\n", isCool)
 }
