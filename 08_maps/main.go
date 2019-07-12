0ddf15c7d09cb9d76c1ba0e6df1609ca6056e5c5
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 15:44:51 -0400

    len

---
+++ 08_maps/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Define map
+	emails := make(map[string]string)
+
+	// Assign kv
+	emails["Bob"] = "bob@gmail.com"
+	emails["Sharon"] = "sharon@gmail.com"
+	fmt.Println(emails)
+	fmt.Println(emails["Bob"])
+}
