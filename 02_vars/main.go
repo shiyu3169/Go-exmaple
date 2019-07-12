6a2676d5f299e5b3b604be07cd3d673781c7f200
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 13:39:21 -0400

    shorhand

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func main() {
 	// Using var
-	var name = "Shiyu"
+
 	var age = 27
+	// Const
 	const isCool = true
+	// Shorthand
+	name := "Shiyu"
 
 	fmt.Println(name, age, isCool)
 	fmt.Printf("%T\n", isCool)
