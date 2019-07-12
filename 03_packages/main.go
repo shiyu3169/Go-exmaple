518e0f6faf986b98cb6ba6ebb5e6446a059d0bb0
Shiyu Wang <shiyu3169@gmail.com>
2019-07-12 14:15:31 -0400

    add your own package

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/shiyu3169/go_example/03_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.7))
 	fmt.Println(math.Ceil(2.7))
 	fmt.Println(math.Sqrt(2.7))
+	fmt.Println(strutil.Reverse("hello"))
 }
