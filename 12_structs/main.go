e4a1ac8e6c88ac261aa1d52cc6b4160d0363dad2
Shiyu Wang <shiyu3169@gmail.com>
2019-07-15 11:39:54 -0400

    value receiver

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // Define person struct
 type Person struct {
@@ -11,10 +14,21 @@ type Person struct {
 	age       int
 }
 
+// Greeting method (value receiver)
+func (p Person) greet() string {
+	return "Hello, my name is " + p.firstName + " " + p.lastName + "and I am " + strconv.Itoa(p.age)
+}
+
 func main() {
 	// Init person using struct
 	// person1 := Person{firstName: "Samantha", lastName: "Smith", city: "Boston", gender: "f", age: 25}
 	person1 := Person{"Samantha", "Smith", "Boston", "f", 25}
 
-	fmt.Println(person1)
+	// fmt.Println(person1)
+	// person1.age++
+	// fmt.Println(person1.firstName)
+
+	// fmt.Println(person1)
+	fmt.Println(person1.greet())
+
 }
