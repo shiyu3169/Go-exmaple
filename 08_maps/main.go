44307fe3a83269969f0ce8acb0eec91c7c272853
Shiyu Wang <shiyu3169@gmail.com>
2019-07-15 09:54:36 -0400

    declare map and kv

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -4,12 +4,20 @@ import "fmt"
 
 func main() {
 	// Define map
-	emails := make(map[string]string)
+	// emails := make(map[string]string)
 
 	// Assign kv
-	emails["Bob"] = "bob@gmail.com"
-	emails["Sharon"] = "sharon@gmail.com"
+	// emails["Bob"] = "bob@gmail.com"
+	// emails["Sharon"] = "sharon@gmail.com"
+
+	// Declare map and kv
+	emails := map[string]string{"Bob": "bob@gmail.com", "Shron": "sharon@gmail.com"}
+
 	fmt.Println(emails)
 	fmt.Println(len(emails))
 	fmt.Println(emails["Bob"])
+
+	// delete from map
+	// delete(emails, "Bob")
+	// fmt.Println(emails)
 }
